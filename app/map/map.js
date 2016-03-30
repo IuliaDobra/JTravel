b625160602e99ae08060386cee6ff65170ee3096
Iulia Dobra <iulia.dobra@gmail.com>
2016-03-30 18:04:06 +0300

    Added routing for maps

--- app/map/map.js
+++ app/map/map.js
@@ -3,18 +3,18 @@
 angular.module('myApp.map', [])
     .controller('MapCtrl',
         function($scope, $log, $timeout) {
+            var directionsService = new google.maps.DirectionsService;
+            var directionsDisplay = new google.maps.DirectionsRenderer;
             if(navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(function(position){
                     $scope.$apply(function () {
                         $scope.position = position;
-                        console.log(position.coords.latitude, position.coords.longitude);
                         $scope.map = new google.maps.Map(document.getElementById('map_canvas'), {
                             zoom: 15,
                             center: {lat: position.coords.latitude, lng: position.coords.longitude},
                             mapTypeControl: true,
                             mapTypeId: google.maps.MapTypeId.ROADMAP
                         });
-
                         $scope.coordsUpdates = 0;
                         $scope.dynamicMoveCtr = 0;
                         $scope.marker = {
@@ -40,6 +40,98 @@ angular.module('myApp.map', [])
                                 }
                             }
                         };
+
+                        $scope.route = function (origin_place_id, destination_place_id, travel_mode,
+                                                 directionsService, directionsDisplay) {
+                            if (!origin_place_id || !destination_place_id) {
+                                return;
+                            }
+                            directionsService.route({
+                                origin: {'placeId': origin_place_id},
+                                destination: {'placeId': destination_place_id},
+                                travelMode: travel_mode
+                            }, function(response, status) {
+                                if (status === google.maps.DirectionsStatus.OK) {
+                                    directionsDisplay.setDirections(response);
+                                    console.log($scope.map);
+                                    //directionsDisplay.setMap($scope.map.control.getGMap());
+                                    directionsDisplay.setPanel(document.getElementById('directionsList'));
+                                    $scope.showList = true;
+                                } else {
+                                    window.alert('Directions request failed due to ' + status);
+                                }
+                            });
+                        };
+
+                        var origin_place_id = null;
+                        var destination_place_id = null;
+                        var travel_mode = google.maps.TravelMode.WALKING;
+                        directionsDisplay.setMap($scope.map);
+
+                        var origin_input = document.getElementById('origin-input');
+                        var destination_input = document.getElementById('destination-input');
+                        var modes = document.getElementById('mode-selector');
+
+                        $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push(origin_input);
+                        $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destination_input);
+                        $scope.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modes);
+
+                        var origin_autocomplete = new google.maps.places.Autocomplete(origin_input);
+                        origin_autocomplete.bindTo('bounds', $scope.map);
+                        var destination_autocomplete = new google.maps.places.Autocomplete(destination_input);
+                        destination_autocomplete.bindTo('bounds',$scope.map);
+
+                        // Sets a listener on a radio button to change the filter type on Places
+                        // Autocomplete.
+                        $scope.setupClickListener = function (id, mode) {
+                            var radioButton = document.getElementById(id);
+                            radioButton.addEventListener('click', function() {
+                                travel_mode = mode;
+                            });
+                        };
+                        $scope.setupClickListener('changemode-walking', google.maps.TravelMode.WALKING);
+                        $scope.setupClickListener('changemode-transit', google.maps.TravelMode.TRANSIT);
+                        $scope.setupClickListener('changemode-driving', google.maps.TravelMode.DRIVING);
+
+                        $scope.expandViewportToFitPlace = function (map, place) {
+                            if (place.geometry.viewport) {
+                                map.fitBounds(place.geometry.viewport);
+                            } else {
+                                map.setCenter(place.geometry.location);
+                                map.setZoom(17);
+                            }
+                        };
+
+                        origin_autocomplete.addListener('place_changed', function() {
+                            var place = origin_autocomplete.getPlace();
+                            if (!place.geometry) {
+                                window.alert("Autocomplete's returned place contains no geometry");
+                                return;
+                            }
+                            $scope.expandViewportToFitPlace($scope.map, place);
+
+                            // If the place has a geometry, store its place ID and route if we have
+                            // the other place ID
+                            origin_place_id = place.place_id;
+                            $scope.route(origin_place_id, destination_place_id, travel_mode,
+                                directionsService, directionsDisplay);
+                        });
+
+                        destination_autocomplete.addListener('place_changed', function() {
+                            var place = destination_autocomplete.getPlace();
+                            if (!place.geometry) {
+                                window.alert("Autocomplete's returned place contains no geometry");
+                                return;
+                            }
+                            $scope.expandViewportToFitPlace($scope.map, place);
+
+                            // If the place has a geometry, store its place ID and route if we have
+                            // the other place ID
+                            destination_place_id = place.place_id;
+                            $scope.route(origin_place_id, destination_place_id, travel_mode,
+                                directionsService, directionsDisplay);
+                        });
+
                         $scope.$watchCollection("marker.coords", function (newVal, oldVal) {
                             if (_.isEqual(newVal, oldVal))
                                 return;
@@ -60,6 +152,14 @@ angular.module('myApp.map', [])
                             }, 2000);
                         }, 1000);
                     })
+                });
+
+            } else {
+                $scope.map = new google.maps.Map(document.getElementById('map_canvas'), {
+                    zoom: 15,
+                    center: {lat: position.coords.latitude, lng: position.coords.longitude},
+                    mapTypeControl: false,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
                 });
             }
         });
