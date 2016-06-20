e00ba0b75c3556aef6ec1484dbe486bb6e5ef081
Iulia Dobra <iulia.dobra@gmail.com>
2016-06-20 14:59:04 +0300

    Fix the itineraries and add angularFire

--- src/components/itinerary/itinerary.controller.js
+++ src/components/itinerary/itinerary.controller.js
@@ -3,16 +3,26 @@ import BaseInjectable from '../../js/BaseInjectable';
 class ItineraryController extends BaseInjectable {
     constructor(...injectables) {
         super(injectables, ItineraryController.$inject);
+        const userId = this.authService.isAuthenticated();
+        const ref = firebase.database().ref('itinerary').child(userId);
+        let syncObject = this.$firebaseObject(ref);
 
-        this.itinerary = this.mapsService.getItinerariesByUserId();
+        syncObject.$bindTo(this.$scope, "itineraries");
+        this.$scope.$watch('itineraries', (r) => {
+            this.itineraries = r;
+            console.log(this.itineraries);}
+        )
     }
 }
 
 ItineraryController.$inject = [
     '$cookies',
     '$state',
+    '$scope',
     'authService',
-    'mapsService'
+    'mapsService',
+    '$firebaseObject',
+
 ];
 
 export default ItineraryController;
