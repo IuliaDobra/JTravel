66ec69d3206a9e7da137e342ccfecf439c1a3045
Alexandru Coman <alex.coman92@yahoo.ro>
2016-06-21 22:49:39 +0300

    google sign in

--- src/components/login/login.controller.js
+++ src/components/login/login.controller.js
@@ -6,6 +6,8 @@ class LoginController extends BaseInjectable {
         this.authEmail = 'test@test.com';
         this.authPassword = 'test';
 
+        this.provider = new firebase.auth.GoogleAuthProvider();
+
     }
 
     createUser() {
@@ -16,6 +18,29 @@ class LoginController extends BaseInjectable {
         if (this.authService.authUser(this.authEmail, this.authPassword)) {
             this.$state.go('master.dashboard');
         }
+    }
+
+    googleAuth() {
+        firebase.auth().signInWithPopup(this.provider).then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+
+            this.$cookies.put('uid', user.uid);
+            this.$state.go('master.dashboard');
+            // ...
+        }).catch(function(error) {
+            console.log(error.message);
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
     }
 }
 
